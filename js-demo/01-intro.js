9769059a59f08fcc99fae018b55bf746feaa4aa0
KriS <kris.gowd@gmail.com>
2016-12-08 09:31:03 +0530

    less demo

---
+++ js-demo/01-intro.js
@@ -0,0 +1,28 @@
+alert('Hey all, welcome!'); //single line comment
+
+var pagetitle = document.getElementById('page-heading');
+
+console.log(pagetitle);
+
+pagetitle.innerHTML = 'What ever';
+
+/*
+	this is a 
+	multiline comment
+*/
+
+var myText = 'This is my Text';
+
+var myNumber = 30;
+var x = '30';
+
+
+if(myNumber === x){
+	alert('Yes they are equal')
+}
+if(x>=31){
+	console.log("Yes x is greater or equal")
+} else{
+	console.log("Yes x is not greater")
+}
+
